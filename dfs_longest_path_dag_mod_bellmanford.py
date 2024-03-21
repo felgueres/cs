97af3b85937fc62b458ce1fed04de27d061b7ff3
felgueres <pablofelgueres@gmail.com>
2024-03-21 07:23:49 -0700

    dfs longest in linear time

--- dfs_longest_path_dag_mod_bellmanford.py
+++ dfs_longest_path_dag_mod_bellmanford.py
@@ -7,7 +7,6 @@
 
 # Note: Edge (x, y, w) represents an edge from x to y having weight w.
 
-
 edges = [(0, 6, 2), (1, 2, -4), (1, 4, 1), (1, 6, 8), (3, 0, 3), (3, 4, 5), (5, 1, 2), (7, 0, 6), (7, 1, -1), (7, 3, 4), (7, 5, -4)]
 n = 8
 src = 7
@@ -25,6 +24,9 @@ def relax(s,d,w):
         dist[d] = dist[s] + w
         pred[d] = s
 
+# OPTION1: BELMAN-FORD O(V*E)
+# complexity of this algorithm is V*E
+
 for i in range(n-1):
     for (o,d,w) in edges:
         relax(o,d,w)
@@ -38,7 +40,25 @@ def print_path(s,d,pred):
         print_path(s, pred[d], pred)
         print(d)
 
-print_path(src,dest,pred)
-
-# complexity of this algorithm is V*E
+# OPTION2: TOPO + ONE PASS OF BELMAN-FORD (V+E)
 # to improve to V+E you'd do topo sort and one pass of the bellmanford instead of n-1
+
+visited = set()
+stack = []
+
+def dfs(s):
+    visited.add(s)
+    for d,_ in adj_l.get(s,[]):
+        if d not in visited:
+            dfs(d)
+    stack.append(s)
+
+for s in range(n):
+    if s not in visited:
+        dfs(s)
+    
+for u in reversed(stack):
+    for v,d in adj_l[u]:
+        relax(u,v,d)
+
+print_path(src,dest,pred)
